1de3da1169c070e8712b63ac83322290fcd265d2
TanerSaydam <tanersaydam@gmail.com>
2024-04-09 04:55:44 +0300

    Infrastructure - Dependency Injection

--- eAppointmentServer/eAppointmentServer.Domain/obj/Debug/net8.0/eAppointmentServer.Domain.AssemblyInfo.cs
+++ eAppointmentServer/eAppointmentServer.Domain/obj/Debug/net8.0/eAppointmentServer.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eAppointmentServer.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8c7e2d40016280da9d932826de0181dd6b5544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28b3b7c618f6c7dbd44dbd807e92dcd0d6040b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("eAppointmentServer.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eAppointmentServer.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
