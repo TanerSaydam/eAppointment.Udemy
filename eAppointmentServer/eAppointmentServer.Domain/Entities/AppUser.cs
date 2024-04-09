6a4b63c1c094e7e6bc6224a739b62c3361f156b7
TanerSaydam <tanersaydam@gmail.com>
2024-04-09 03:10:42 +0300

    Domain katmanı ve AppUser

---
+++ eAppointmentServer/eAppointmentServer.Domain/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eAppointmentServer.Domain.Entities;
+public sealed class AppUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => string.Join(" ", FirstName, LastName);
+}
