180cb8b394f89ae4e9c7756164ea2e0c0c3cf839
TanerSaydam <tanersaydam@gmail.com>
2024-04-09 05:03:20 +0300

    Entity Framework Core ile Context Yapısı Oluşturma ve DI

---
+++ eAppointmentServer/eAppointmentServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using eAppointmentServer.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAppointmentServer.Infrastructure.Context;
+internal sealed class ApplicationDbContext : IdentityDbContext<AppUser,AppRole,Guid>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
--- eAppointmentServer/eAppointmentServer.Infrastructure/DependencyInjection.cs
+++ eAppointmentServer/eAppointmentServer.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eAppointmentServer.Infrastructure;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, 
+        IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+        });
+
+        services.AddIdentity<AppUser, AppRole>(action =>
+        {
+            action.Password.RequiredLength = 1;
+            action.Password.RequireUppercase = false;
+            action.Password.RequireLowercase = false;
+            action.Password.RequireNonAlphanumeric = false;
+            action.Password.RequireDigit = false;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
+
         return services;
     }
 }
