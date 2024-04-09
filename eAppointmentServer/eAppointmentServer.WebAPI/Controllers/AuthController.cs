27d85d95d1add7fede067f049d05738ba149f4ae
TanerSaydam <tanersaydam@gmail.com>
2024-04-09 06:11:07 +0300

    Login Endpoint

---
+++ eAppointmentServer/eAppointmentServer.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using eAppointmentServer.Application.Features.Auth.Login;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAppointmentServer.WebAPI.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public sealed class AuthController(
+    IMediator mediator): ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var response = await mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
