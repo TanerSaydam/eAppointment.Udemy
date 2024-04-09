bad4831820a2df2722005e58b450416678ddbe45
TanerSaydam <tanersaydam@gmail.com>
2024-04-09 03:15:46 +0300

    Entity - Patient

---
+++ eAppointmentServer/eAppointmentServer.Domain/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace eAppointmentServer.Domain.Entities;
+public sealed class Patient
+{
+    public Patient()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string IdentityNumber { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Town { get; set; } = string.Empty;
+    public string FullAddress { get; set; } = string.Empty;
+}
