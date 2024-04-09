82d0efb5ebfc069d869c6a7e6152f64ac74d9830
TanerSaydam <tanersaydam@gmail.com>
2024-04-09 04:04:05 +0300

    MediatR Kütüphanesi ve DI

--- eAppointmentServer/eAppointmentServer.Application/DependencyInjection.cs
+++ eAppointmentServer/eAppointmentServer.Application/DependencyInjection.cs
@@ -5,6 +5,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
         return services;
     }
 }
