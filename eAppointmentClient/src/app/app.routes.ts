6a1d718bdae5707558c4153e3c272637493d9c9a
TanerSaydam <tanersaydam@gmail.com>
2024-04-10 21:50:22 +0300

    Layout yapısı ve routing

--- eAppointmentClient/src/app/app.routes.ts
+++ eAppointmentClient/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: LayoutsComponent,
+        children: [
+            {
+                path: "",
+                component: HomeComponent
+            }
+        ]
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];
