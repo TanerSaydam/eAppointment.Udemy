74a9191aec148376c5a6a6452aa1e05b061ce6c4
TanerSaydam <tanersaydam@gmail.com>
2024-04-10 23:39:59 +0300

    Navbar tasarımı

--- eAppointmentClient/src/app/components/layouts/layouts.component.ts
+++ eAppointmentClient/src/app/components/layouts/layouts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
 })
---
+++ eAppointmentClient/src/app/components/layouts/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  constructor(
+    private router: Router
+  ){}
+
+  signOut(){
+    localStorage.removeItem("token");
+    this.router.navigateByUrl("/login");
+  }
+}
